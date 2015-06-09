89c53d56cab5ca303b384a5e19b055a06aa3d575
toomanyadverbs <lark.grimes@gmail.com>
2015-06-09 16:53:13 -0400

    Adds change-background-color dropdown option

--- js/index.js
+++ js/index.js
@@ -1,32 +1,29 @@
-$document.jQuery(document).ready(function($) {
+$(document).ready(function(){
 
   $('button').click(function(){
+    $('body').css('background-color', $('#drop_down_button').val());
 
-var turquoise = '#1abc9c'
-var greensea = '#16a085'
-var sunflower = '#f1c40f'
-var orange = '#f39c12'
-var emerald = '#2ecc71'
-var nephritis = '#27ae60'
-var carrot = '#e67e22'
-var pumpkin = '#d35400'
-var peterriver = '#3498db'
-var belizehole = '#d35400'
-var alizarin = '#d35400'
-var pomegranate = '#c0392b'
-var amethyst = '#9b59b6'
-var wisteria = '#8e44ad'
-var clouds = '#ecf0f1'
-var wetasphalt = '#34495e'
-var midnightblue = '#2c3e50'
-var concrete = '#95a5a6'
-var asbestos = '#7f8c8d'
-
-
-var colorChoice = $(# /*the color selected from the drop-down menu*/)
-
-$('body').style.append('background-color:' + colorChoice)
+// var turquoise = '#1abc9c'
+// var greensea = '#16a085'
+// var sunflower = '#f1c40f'
+// var orange = '#f39c12'
+// var emerald = '#2ecc71'
+// var nephritis = '#27ae60'
+// var carrot = '#e67e22'
+// var pumpkin = '#d35400'
+// var peterriver = '#3498db'
+// var belizehole = '#d35400'
+// var alizarin = '#d35400'
+// var pomegranate = '#c0392b'
+// var amethyst = '#9b59b6'
+// var wisteria = '#8e44ad'
+// var clouds = '#ecf0f1'
+// var wetasphalt = '#34495e'
+// var midnightblue = '#2c3e50'
+// var concrete = '#95a5a6'
+// var asbestos = '#7f8c8d'
 
 
-  }); //click
+
+  }); //select
 }); //document ready
